67fc1b23ecc20d3d96d7ccf2000859e85dcf0253
WolfgangYang <87144214+WolfgangYang@users.noreply.github.com>
2026-09-02 15:43:50 +0800

    Update Config.h

--- src/Config/Config.h
+++ src/Config/Config.h
@@ -1,24 +1,16 @@
-// Configuration Module
 #pragma once
-
 #include <string>
 
 class Config {
 public:
     enum class Language { Chinese, English };
-    
     static void Initialize();
-    static void LoadFromFile(const std::wstring& path);
-    static void SaveFromFile(const std::wstring& path);
-    
     static Language GetLanguage();
     static int GetBrightness();
     static int GetSpeed();
-    
-    static void SetLanguage(Language lang);
     static void SetBrightness(int value);
     static void SetSpeed(int value);
-    
+
 private:
     static Language language;
     static int brightness;
