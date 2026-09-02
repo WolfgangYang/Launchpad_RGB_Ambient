44e697f227be8b40e27f5e9da002c2f98532c185
WolfgangYang <87144214+WolfgangYang@users.noreply.github.com>
2026-09-02 15:40:31 +0800

    Update LEDController.h

--- src/LED/LEDController.h
+++ src/LED/LEDController.h
@@ -1,17 +1,10 @@
-// LED Controller Module
 #pragma once
-
 #include <windows.h>
-#include "MIDI/MIDIDevice.h"
+#include "../MIDI/MIDIDevice.h"
 
 class LEDController {
 public:
-    struct Position { int x, y; };
-    
-    static void SetLED(int x, int y, BYTE red, BYTE green, BYTE blue);
+    static void SetLED(int x, int y, BYTE r, BYTE g, BYTE b);
     static void ClearAll();
     static BYTE PositionToIndex(int x, int y);
-    
-private:
-    static void SendRGBMessage(BYTE ledIndex, BYTE red, BYTE green, BYTE blue);
 };
